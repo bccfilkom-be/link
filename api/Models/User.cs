48c575fe79f3d902e0834c7e30ec72c35332e0db
Naufal Haris <naufal.michiels@gmail.com>
2024-10-23 16:58:49 +0700

    feat(model): add model for user

---
+++ api/Models/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Column(TypeName = "uuid")]
+        public Guid id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public string? Device { get; set; }
+        public string? PhotoProfileUri { get; set; }
+
+    }
+}
